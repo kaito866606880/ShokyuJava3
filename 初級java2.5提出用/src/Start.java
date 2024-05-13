206f3a03b38a024cadf588ef15ef886743b69296
kaito <zeal23310083@fsgcl.jp>
2024-05-13 11:55:25 +0900

    初級java2.5提出

---
+++ 初級java2.5提出用/src/Start.java
@@ -0,0 +1,20 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		
+		
+		
+		MyFrame frame2=new RectFrame();
+		frame2.setLocation(350,200);
+		MyFrame frame3=new RectFrame();
+		frame3.setLocation(250,200);
+		
+		int x = 2;
+		int y = 3;
+		System.out.println(x*y);
+		
+
+	}
+
+}
