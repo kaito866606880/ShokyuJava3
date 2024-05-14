0517ef330b12972348189b03633faf94f2138775
kaito <zeal23310083@fsgcl.jp>
2024-05-14 10:48:00 +0900

    3.1

--- 初級java2.5提出用/src/Start.java
+++ 初級java2.5提出用/src/Start.java
@@ -2,17 +2,11 @@
 public class Start {
 
 	public static void main(String[] args) {
+		System.out.println("プログラムを実行します");
+		MyFrame frame1 = new RectTest();
 		
 		
 		
-		MyFrame frame2=new RectFrame();
-		frame2.setLocation(350,200);
-		MyFrame frame3=new RectFrame();
-		frame3.setLocation(250,200);
-		
-		int x = 2;
-		int y = 3;
-		System.out.println(x*y);
 		
 
 	}
