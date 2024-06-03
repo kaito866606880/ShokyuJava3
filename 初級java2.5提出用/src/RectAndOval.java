c4252ce29237da7237f55e364856129e9db2b198
kaito <zeal23310083@fsgcl.jp>
2024-06-03 10:34:35 +0900

    ４－４の確認問題。　　　3-4から4-3までの確認問題はcheck4-4にあります

---
+++ 初級java2.5提出用/src/RectAndOval.java
@@ -0,0 +1,10 @@
+
+public class RectAndOval extends MyFrame {
+	public void run() {
+		fillRect(60,60,30,30);
+		fillRect(40,80,70,20);
+		fillOval(50,100,20,20);
+		fillOval(80,100,20,20);
+	}
+
+}
