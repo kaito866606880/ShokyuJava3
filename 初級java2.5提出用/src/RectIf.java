d351b9c464a5d7098104a8971c78fc8e6d0137d6
kaito <zeal23310083@fsgcl.jp>
2024-06-03 10:45:02 +0900

    ４－２練習問題

--- 初級java2.5提出用/src/RectIf.java
+++ 初級java2.5提出用/src/RectIf.java
@@ -1,14 +1,16 @@
 
 public class RectIf extends MyFrame{
 	public void run() {
-		int x = 30;
+		int x = 10;
 		setColor(0,128,0);
 		
 		int i;
 		for (i=0; i<9;i++) {
 							
-			fillRect(x,x,10,100);
+			fillRect(x,100,10,x);
 			x+=20;
+			
+			//横縦幅高さ
 		}
 	}
 }
