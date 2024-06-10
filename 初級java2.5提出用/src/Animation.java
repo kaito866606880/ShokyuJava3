32210cbb58e797f24a5b5925260420c154fe0753
kaito <zeal23310083@fsgcl.jp>
2024-06-10 11:36:20 +0900

    4-３練習問題

--- 初級java2.5提出用/src/Animation.java
+++ 初級java2.5提出用/src/Animation.java
@@ -1,16 +1,17 @@
 
-public class RectIf extends MyFrame{
+public class Animation extends MyFrame{
 	public void run() {
 		int x = 10;
-		int a = 128;
-		setColor(0,128,0);
+		int a = 0;
+		
 		
 		int i =0;
 		for (i=0; i<9;i++) {
 							
 			fillRect(x,100,10,100);
-			setColor(0,0,0);
-			x+=20;
+			setColor(a,a,a);
+			a+=20;
+			x += 20;
 			
 			//横縦幅高さ
 		}
