03162dcc72267a8cb1aa0bfef90b0517fa2990fe
kaito <zeal23310083@fsgcl.jp>
2024-06-11 09:57:36 +0900

    4-4

--- 初級java2.5提出用/src/RectIf.java
+++ 初級java2.5提出用/src/RectIf.java
@@ -3,14 +3,11 @@ public class RectIf extends MyFrame{
 	public void run() {
 		int x = 30;
 		setColor(0,128,0);
-		
+
 		int i;
 		for(i=0; i<9;i++) {
-			if(i == 0) {
+			if(i >= 4 && i <= 7) {
 				fillRect(x,100,10,100);
-			}
-			else if(i==8) {
-				fillRect(x,60,10,100);
 			}
 			else {
 				fillRect(x,80,10,100);				
