4e1eb207f2cd5e8548f43f2c1323aae07c99af68
kaito <zeal23310083@fsgcl.jp>
2024-06-11 10:10:00 +0900

    ５－２

--- 初級java2.5提出用/src/DisplayRectsSlow.java
+++ 初級java2.5提出用/src/DisplayRectsSlow.java
@@ -1,12 +1,18 @@
 
 public class DisplayRectsSlow extends MyFrame{
 	public void run() {
-	setColor(0,128,0);
-	int x= 30;
-	while(x<=170) {
-		fillRect(x,80,10,100);
-		x+=20;
-		sleep(1);
+		int x = 30;
+
+		while(x < 170) {
+
+			clear();
+
+			setColor(0,128,0);
+			fillRect(x,80,10,100);
+
+			x+=5;
+
+			sleep(0.1);
 		
 	}
 	
