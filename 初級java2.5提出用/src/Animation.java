6ae1e9f5cd7d19f38ac7cf012894858523cfbaf0
kaito <zeal23310083@fsgcl.jp>
2024-06-11 10:47:02 +0900

    ５－１追加課題

--- 初級java2.5提出用/src/Animation.java
+++ 初級java2.5提出用/src/Animation.java
@@ -1,6 +1,9 @@
 public class Animation extends MyFrame {
 	int y = 30;
 	int x = 30;
+
+	int nextY = 0;
+
 	private void MoveRect(int _x, int _y) {
 		//(1)画面を消す
 		clear();
@@ -14,15 +17,27 @@ public class Animation extends MyFrame {
 
 	public void run() {
 
-		for (int i = 0; i < 3; i++) {
+		while (true) {
+
 			while (x <= 200) {
 
 				MoveRect(5, 0);
 			}
 
+			nextY = y + 50;
+			while (y <= nextY) {
+				MoveRect(0, 5);
+			}
+
 			while (x >= 30) {
 
 				MoveRect(-5, 0);
+			}
+
+			nextY = y + 50;
+			while (y <= nextY) {
+
+				MoveRect(0, 5);
 			}
 		}
 	}
