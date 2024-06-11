4815d8b5d8940affb82f42b36413862f13e4ac63
kaito <zeal23310083@fsgcl.jp>
2024-06-11 11:01:25 +0900

    ６－１

--- 初級java2.5提出用/src/Start.java
+++ 初級java2.5提出用/src/Start.java
@@ -1,13 +1,10 @@
 
 public class Start {
-
 	public static void main(String[] args) {
-		
+		// TODO 自動生成されたメソッド・スタブ
+
 		System.out.println("プログラムを実行します");
-		MyFrame frame1 = new Animation();
-		
-		
+		MyFrame frame1 = new DisplayFaceObject();
 
 	}
-
 }
