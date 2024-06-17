9afebd71d3f3dc477bcb1fee1d6bee1f41c647a7
kaito <zeal23310083@fsgcl.jp>
2024-06-17 09:55:17 +0900

    6-3

--- 初級java2.5提出用/src/DisplayFaceObject.java
+++ 初級java2.5提出用/src/DisplayFaceObject.java
@@ -1,13 +1,22 @@
 
 public class DisplayFaceObject extends MyFrame{
 	public void run() {
-		Face face1 = new Face(25,50);
-		face1.draw(this);
+		Face face1 = new Face(50, 50);
 
-		Face face2 = new Face(125,150);
-		face2.draw(this);		
+		Face face2 = new Face(200, 100);
 
-		Face face3 = new Face(225,50);
-		face3.draw(this);
+		for (int i = 0; i < 30; i++) {
+
+			clear();
+			face1.draw(this);
+			face2.draw(this);
+
+			face1.x += 10;
+			face1.y += 5;
+			face2.x -= 10;
+			face2.y -= 5;
+
+			sleep(0.1);
+		}
 	}
 }
