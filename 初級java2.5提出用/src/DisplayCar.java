6ece8b6dc4895595abd8530a37afd7c997f3f02e
kaito <zeal23310083@fsgcl.jp>
2024-06-18 10:51:40 +0900

    6-2練習問題

--- 初級java2.5提出用/src/DisplayCar.java
+++ 初級java2.5提出用/src/DisplayCar.java
@@ -2,8 +2,20 @@
 public class DisplayCar extends MyFrame {
 	public void run() {
 
-		Car car1 = new Car(100, 100, 100, 0);
+		Car car1 = new Car(0, 50, 10, 0);
 
-		car1.draw(this);
+		Car car2 = new Car(400, 100, -10, -0);
+
+		for (int i = 0; i < 30; i++) {
+
+			clear();
+			car1.draw(this);
+			car2.draw(this);
+
+			car1.move();
+			car2.move();
+
+			sleep(0.1);
+		}
 	}
 }
