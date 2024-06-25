bb5233f6875a23afb30bd86dcdd185fc172bebc2
kaito <zeal23310083@fsgcl.jp>
2024-06-25 10:13:14 +0900

    ８－４

--- 初級java2.5提出用/src/Vehicle.java
+++ 初級java2.5提出用/src/Vehicle.java
@@ -1,7 +1,6 @@
 
 public class Vehicle {
 	int x, y, vx, vy;
-
 	public Vehicle(int x, int y, int vx, int vy) {
 		this.x = x;
 		this.y = y;
@@ -9,8 +8,14 @@ public class Vehicle {
 		this.vy = vy;
 
 	}
+
 	public void move() {
 		x += vx;
 		y += vy;
+	}
+
+	public void draw(MyFrame f) {
+		// TODO 自動生成されたメソッド・スタブ
+
 	}
 }
