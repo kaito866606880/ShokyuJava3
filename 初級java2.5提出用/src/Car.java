ee0925ca2eae7e1d5fc3e2d2f43dc5c6fb061cee
kaito <zeal23310083@fsgcl.jp>
2024-06-25 09:55:57 +0900

    ８－１

--- 初級java2.5提出用/src/Car.java
+++ 初級java2.5提出用/src/Car.java
@@ -1,20 +1,23 @@
 
 public class Car {
 	int x,y,vx,vy;
-	public Car(int x,int y,int vx,int vy) {
+	public Car(int x,int y, int vx,int vy) {
 		this.x = x;
 		this.y = y;
-		this.vx=vx;
-		this.vy=vy;
-	}
-	public void draw(MyFrame frame) {
-		frame.fillOval(x + 10, y+20, 30, 30);
-		frame.fillOval(x + 60, y+20, 30, 30);
-		frame.fillRect(x, y, 100, 30);
-		frame.fillRect(x+25, y-20, 50, 50);
-	}
-	public void move() {
-		x = x + vx + vy;
+		this.vx = vx;
+		this.vy = vy;
+
 	}
 
+	public void draw(MyFrame frame) {
+		frame.fillRect(x+20,y,40,20);
+		frame.fillRect(x,y + 20,80,20);
+		frame.fillOval(x+10,y+40,20,20);
+		frame.fillOval(x+50,y+40,20,20);
+	}
+
+	public void move() {
+		x += vx;
+		y += vy;
+	}
 }
